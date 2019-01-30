7d5d609c0f8ecc3eb0b977eea500fa81ac88129d
Drake Fithen <df975329@wtstudents.wtamu.edu>
2019-01-30 16:10:48 -0600

    Initial commit

---
+++ MessagePrinter/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MessagePrinter
+{
+    public class MessagePrintingService
+    {
+        public string Message { get; set; } = "Default";
+
+        public MessagePrintingService()
+        {
+
+        }
+        public MessagePrintingService(string message)
+        {
+            this.Message = message;
+        }
+        public void PrintMessage()
+        {
+            Console.WriteLine(this.Message);
+        }
+    }
+}
